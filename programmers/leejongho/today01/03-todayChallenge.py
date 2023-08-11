253d7b25b76a7b3fe83dd612d000306f25155cb0
bellDev-code <sumaoo20@naver.com>
2023-08-11 09:38:04 +0900

    [PGS]a,b 문자열 출력/03/이종호

---
+++ programmers/leejongho/today01/03-todayChallenge.py
@@ -0,0 +1,9 @@
+# a, b 문자열 출력
+# 파이썬 변수할당 방법
+# strip() 문자열 공백 제거 함수
+# split() 문자열 공백을 기준으로 나눔
+a, b = input().strip().split(' ')
+count = int(b)
+# 문자열과 정수 곱한 결과
+result = a * count
+print(result)
