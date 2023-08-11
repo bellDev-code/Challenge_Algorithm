b04a145b2e5b756f8f13c11b25a67c02e64dca1b
Oh-SulGi <dhtmfrl0615@naver.com>
2023-08-11 17:54:30 +0900

    [PGS] 1로 만들기/10/오슬기

---
+++ programmers/Ohsulgi/today01/10-todayChallenge.py
@@ -0,0 +1,20 @@
+# [PGS] 1로 만들기/10/오슬기
+
+def solution(num_list):
+    arr = [1 for i in range(len(num_list))]
+    cnt = 0
+    i = 0
+    while True:
+        if arr == num_list:
+            return cnt
+            break
+
+        if num_list[i] != 1:
+            if num_list[i] % 2 == 0:
+                num_list[i] = int(num_list[i] / 2)
+                cnt += 1
+            else:
+                num_list[i] = int((num_list[i] - 1) / 2)
+                cnt += 1
+        else:
+            i += 1
