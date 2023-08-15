238bf7d1096f7997897ea4b23683c8d9f3b84fe0
Oh-SulGi <dhtmfrl0615@naver.com>
2023-08-15 19:13:03 +0900

    [PGS] 간단한 식 계산하기/04/오슬기

--- programmers/Ohsulgi/today03/04-todayChallenge.py
+++ programmers/Ohsulgi/today03/04-todayChallenge.py
@@ -1,13 +1,13 @@
 # [PGS] 간단한 식 계산하기/04/오슬기
 
-binomial = "43 + 12"
+def solution(binomial):
+    arr = binomial.split()
 
-arr = binomial.split()
-print(arr)
-
-if arr[1] == '+':
-    answer = int(arr[0]) + int(arr[-1])
-elif arr[1] == '-':
-    answer = int(arr[0]) - int(arr[-1])
-else:
-    answer = int(arr[0]) * int(arr[-1])
+    if arr[1] == '+':
+        answer = int(arr[0]) + int(arr[-1])
+    elif arr[1] == '-':
+        answer = int(arr[0]) - int(arr[-1])
+    else:
+        answer = int(arr[0]) * int(arr[-1])
+        
+    return answer
