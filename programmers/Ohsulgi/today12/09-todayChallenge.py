50e99464327b455c48c9681242dd6d2f49b78dde
Oh-SulGi <dhtmfrl0615@naver.com>
2023-08-25 20:39:59 +0900

    [BOJ] 14264 주식 투자/09/오슬기

---
+++ programmers/Ohsulgi/today12/09-todayChallenge.py
@@ -0,0 +1,18 @@
+'''
+[BOJ] 14264 주식 투자/09/오슬기
+
+각각의 날짜에 최대값이 0보다 크다면 더하고 아니면 패스한다
+
+'''
+
+import sys
+sys.stdin = open('D:\data\백준\input.txt', 'r')
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    sum_ = 0
+    for _ in range(int(input())):
+        a, b, c = map(int, input().split())
+        if max(a, b, c) >= 0:
+            sum_ += max(a, b, c)
+    print(sum_)
